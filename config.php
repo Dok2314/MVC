56a1c6f7466fd5f5d70bd179a663fcd272a8c675
Dok2314 <koversundanil@gmail.com>
2022-07-06 22:15:35 +0300

    Added base settings

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die('Access Denied');
+
+const SITE_URL = 'http://laravelmvc:8889/';
+const PATH     = '/';
+
+const HOST    = 'localhost';
+const USER    = 'root1';
+const PASS    = 'root';
+const DB_NAME = 'mvc_like_laravel';
