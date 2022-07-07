d55c993f61aa671051a124189816550f2fbb4fc6
Dok2314 <koversundanil@gmail.com>
2022-07-07 18:28:53 +0300

    Added Singleton RouteController

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ini_set('error_reporting', E_ALL);
 
 const VG_ACCESS = true;
 
@@ -7,3 +10,13 @@ session_start();
 
 require_once 'config.php';
 require_once 'core/base/settings/internal_settings.php';
+
+use core\base\exceptions\RouteException;
+use core\base\controllers\RouteController;
+
+try{
+//    RouteController::getInstance()->route();
+
+}catch (RouteException $e) {
+    exit($e->getMessage());
+}
