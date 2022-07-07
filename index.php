f69c0a28190216ada61a2c193f37d17f97d0d183
Dok2314 <koversundanil@gmail.com>
2022-07-07 11:58:07 +0300

    Changes

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-ini_set('error_reporting', E_ALL);
+
 const VG_ACCESS = true;
 
 header('Content-Type:text/html;charset=utf-8');
@@ -9,12 +7,3 @@ session_start();
 
 require_once 'config.php';
 require_once 'core/base/settings/internal_settings.php';
-
-function loadClass($className) {
-    $className = str_replace('\\', '/', $className);
-    include $className . '.php';
-}
-
-spl_autoload_register('loadClass');
-
-(new \n2\B());
